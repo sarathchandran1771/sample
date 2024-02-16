b560b8a00b41f2f8b588c684f5e59c0581ba0390
Sarath Chandran <sarathchandran1771@gmail.com>
2024-02-16 13:44:34 +0530

    cors Updated

--- shared/utilities/corsManage.js
+++ shared/utilities/corsManage.js
@@ -3,21 +3,14 @@ const express = require("express");
 const cors = require('cors');
 const corsManage = express.Router();
 
-// Allow only specific origins
-const allowedOrigins = ['https://connectloom.shop', 'https://localhost:3000'];
 
-// Move the cors middleware to be applied 
-corsManage.use(cors({
+const corsOptions = {
     credentials: true,
-    origin: function (origin, callback) {
-        // Check if the origin is in the allowedOrigins array or if it's undefined (allow if same-origin)
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: '*',
     methods: 'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-}));
+};
+
+corsManage.use(cors(corsOptions));
 
 module.exports = corsManage;
+
